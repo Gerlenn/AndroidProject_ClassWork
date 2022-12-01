9d526ff3ea52e587d4d97c4e37b10fa81d6564ed
Gerlenn <gleb_kozlov1998@mail.ru>
2022-12-01 22:08:46 +0300

    Lesson 22

--- app/src/main/java/com/example/androidproject/ItemsFragment.kt
+++ app/src/main/java/com/example/androidproject/ItemsFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidproject.adapter.ItemsAdapter
@@ -15,6 +18,8 @@ import com.example.androidproject.model.ItemsModel
 class ItemsFragment : Fragment(), itemListener {
 
     private lateinit var itemsAdapter: ItemsAdapter // private var itemsAdapter: ItemsAdapter = ItemsAdapter() можно так но не нужно
+
+    private val viewModel: ItemsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,80 +36,40 @@ class ItemsFragment : Fragment(), itemListener {
         recyclerView.layoutManager = LinearLayoutManager(context)//requireContext() или requireActivity()
         recyclerView.adapter = itemsAdapter
 
-        val listItems = listOf<ItemsModel>(
-            ItemsModel(R.drawable.android,
-                "Android",
-                "23.12.2022"
-            ),
-            ItemsModel(R.drawable.swift,
-                "IOS",
-                "23.12.2022"
-            ),
-            ItemsModel(R.drawable.flutter,
-                "Flutter",
-                "23.12.2022"
-            ),
-            ItemsModel(R.drawable.pyth,
-                "Python",
-                "23.12.2022"
-            ),
-            ItemsModel(R.drawable.cplus,
-                "C++",
-                "23.12.2022"
-            ),
-            ItemsModel(R.drawable.c,
-                "C",
-                "23.12.2022"
-            ),
-            ItemsModel(R.drawable.ruby,
-                "Ruby",
-                "23.12.2022"
-            ),
-            ItemsModel(R.drawable.net,
-                ".NET",
-                "03.01.2018"
-            ),
-            ItemsModel(R.drawable.go,
-                "Golang",
-                "23.12.2030"
-            ),
-            ItemsModel(R.drawable.php,
-                "PHP",
-                "23.12.2008"
-            ),
-            ItemsModel(R.drawable.js,
-                "JS",
-                "20.09.2006"
-            ),
-            ItemsModel(R.drawable.fruit,
-                "React",
-                "23.12.1992"
-            )
-        )
-        itemsAdapter.submitList(listItems)
+        viewModel.getData()
+
+        viewModel.items.observe(viewLifecycleOwner){ listItems -> //получаем данные из viewModel
+            itemsAdapter.submitList(listItems)
+        }
+
+        viewModel.msg.observe(viewLifecycleOwner){ msg ->
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+        }
+
+        viewModel.bundle.observe(viewLifecycleOwner){navBundle ->
+            val  detailsFragment = DetailsFragment()
+            val bundle = Bundle()
+            bundle.putString("name", navBundle.name)
+            bundle.putString("date", navBundle.date)
+            bundle.putInt("imageView", navBundle.image)
+            detailsFragment.arguments = bundle
+
+            Toast.makeText(context, "called", Toast.LENGTH_SHORT).show()
+
+            parentFragmentManager
+                .beginTransaction()
+                .replace(R.id.activity_container, detailsFragment)
+                //.add(R.id.activity_container, detailsFragment)
+                .addToBackStack("Details")
+                .commit()
+        }
     }
 
     override fun onClick() {
-        Toast.makeText(context, "ImageView clicked", Toast.LENGTH_SHORT).show()
+        viewModel.imageViewClicked()
     }
 
     override fun onElementSelected(name: String, date: String, imageView: Int) {
-
-        val  detailsFragment = DetailsFragment()
-        val bundle = Bundle()
-        bundle.putString("name", name)
-        bundle.putString("date", date)
-        bundle.putInt("imageView", imageView)
-        detailsFragment.arguments = bundle
-
-
-        // add метод мы больше не испульзуем, теперь мы будем использовать replace
-        //replace всегда будет иметь или addToBackstack, чтобы мы могли вернуться назад
-        parentFragmentManager
-            .beginTransaction()
-            .replace(R.id.activity_container, detailsFragment)
-            //.add(R.id.activity_container, detailsFragment)
-            .addToBackStack("Details")
-            .commit()
+        viewModel.elementClicked(name, date, imageView)
     }
 }
